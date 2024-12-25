8eec87cab80821fa6c6a9eec6a8c5650d5c63bc4
Stefan Ferstl <st.ferstl@gmail.com>
2024-12-25 21:26:17 +0100

    Introduce simple command line interface
    Signed-off-by: Stefan Ferstl <st.ferstl@gmail.com>

--- src/main/java/com/github/ferstl/skydemonexporter/ExporterRunner.java
+++ src/main/java/com/github/ferstl/skydemonexporter/ExporterRunner.java
@@ -6,23 +6,52 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
+import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-public class ExporterRunner {
+public class ExporterRunner implements Callable<Integer> {
 
-  public static final Pattern UUID_PATTERN = Pattern.compile("[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}");
+  private static final Pattern UUID_PATTERN = Pattern.compile("[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}");
+
+  @Option(
+      names = {"-h", "--help"},
+      usageHelp = true,
+      description = "Display this help message"
+  )
+  private boolean helpRequested;
+
+  @Option(
+      names = {"-d", "--data"},
+      paramLabel = "DIRECTORY",
+      description = "Directory containing the SkyDemon files and the index.xml"
+  )
+  private String platesDir;
+
+  @Parameters(
+      paramLabel = "TARGET",
+      description = "Directory to export the data. It will be created if it does not exist.")
+  private String targetDir;
 
   public static void main(String[] args) throws Exception {
-    Path platesDir = findSkyDemonData();
-    Path targetDir = Paths.get(System.getProperty("user.home"), "Downloads", "SkyDemon");
+    int status = new CommandLine(new ExporterRunner()).execute(args);
+    System.exit(status);
+  }
 
+  @Override
+  public Integer call() throws Exception {
+    Path platesDir = this.platesDir != null ? Path.of(expandUserHome(this.platesDir)) : findSkyDemonData();
+    Path targetDir = Path.of(expandUserHome(this.targetDir));
     Properties airfieldNames = readAirfieldNames();
 
     new Exporter(airfieldNames, platesDir, targetDir).export();
+    return 0;
   }
 
-  public static Path findSkyDemonData() throws IOException {
+  private static Path findSkyDemonData() throws IOException {
     Path targetDir = Paths.get(System.getProperty("user.home"), "Library", "Containers");
 
     try (Stream<Path> files = Files.list(targetDir)) {
@@ -39,7 +68,7 @@ public class ExporterRunner {
     }
   }
 
-  public static Properties readAirfieldNames() {
+  private static Properties readAirfieldNames() {
     Properties properties = new Properties();
     try {
       properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("airfields.properties"));
@@ -47,5 +76,9 @@ public class ExporterRunner {
       throw new UncheckedIOException(e);
     }
     return properties;
+  }
+
+  private static String expandUserHome(String path) {
+    return path.replaceFirst("^~", System.getProperty("user.home"));
   }
 }
