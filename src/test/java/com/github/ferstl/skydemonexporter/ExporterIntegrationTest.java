59fa44ae90f8bbbd536adfab5650a48ba4330edf
Stefan Ferstl <st.ferstl@gmail.com>
2024-12-29 16:40:00 +0100

    Setup integration test with test data
    Signed-off-by: Stefan Ferstl <st.ferstl@gmail.com>

---
+++ src/test/java/com/github/ferstl/skydemonexporter/ExporterIntegrationTest.java
@@ -0,0 +1,16 @@
+package com.github.ferstl.skydemonexporter;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import static com.github.ferstl.skydemonexporter.EnvironmentUtil.readAirfieldNames;
+
+class ExporterIntegrationTest {
+
+  @Test
+  void integrationTest(@TempDir Path tempDir) throws Exception {
+    Exporter exporter = new Exporter(readAirfieldNames(), Paths.get("etc/testdata"), tempDir);
+    exporter.export();
+  }
+}
