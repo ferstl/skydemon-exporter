fd4e85a023dad56174a8311bb12fd93772b1a108
Stefan Ferstl <st.ferstl@gmail.com>
2024-12-29 16:34:23 +0100

    Extract utility for environment stuff
    Signed-off-by: Stefan Ferstl <st.ferstl@gmail.com>

--- src/main/java/com/github/ferstl/skydemonexporter/ExporterRunner.java
+++ src/main/java/com/github/ferstl/skydemonexporter/ExporterRunner.java
@@ -1,21 +1,15 @@
 package com.github.ferstl.skydemonexporter;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.concurrent.Callable;
-import java.util.regex.Pattern;
-import java.util.stream.Stream;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import static com.github.ferstl.skydemonexporter.EnvironmentUtil.expandUserHome;
+import static com.github.ferstl.skydemonexporter.EnvironmentUtil.readAirfieldNames;
 
 public class ExporterRunner implements Callable<Integer> {
-
-  private static final Pattern UUID_PATTERN = Pattern.compile("[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}");
 
   @Option(
       names = {"-h", "--help"},
@@ -43,7 +37,7 @@ public class ExporterRunner implements Callable<Integer> {
 
   @Override
   public Integer call() throws Exception {
-    Path platesDir = this.platesDir != null ? Path.of(expandUserHome(this.platesDir)) : findSkyDemonData();
+    Path platesDir = (this.platesDir != null) ? Path.of(expandUserHome(this.platesDir)) : EnvironmentUtil.findSkyDemonData();
     Path targetDir = Path.of(expandUserHome(this.targetDir));
     Properties airfieldNames = readAirfieldNames();
 
@@ -51,57 +45,4 @@ public class ExporterRunner implements Callable<Integer> {
     return 0;
   }
 
-  private static Path findSkyDemonData() throws IOException {
-    String os = System.getProperty("os.name");
-
-    if (os.toLowerCase().startsWith("mac")) {
-      return findSkyDemonDataForMac();
-    }
-    if (os.toLowerCase().startsWith("windows")) {
-      return findSkyDemonDataForWindows();
-    }
-
-    throw new IllegalStateException("Unsupported OS: " + os);
-  }
-
-  private static Path findSkyDemonDataForMac() throws IOException {
-    Path dataDir = Paths.get(System.getProperty("user.home"), "Library", "Containers");
-
-    try (Stream<Path> files = Files.list(dataDir)) {
-      Path candidateDirectory = files
-          .filter(Files::isReadable)
-          .filter(Files::isDirectory)
-          .filter(directory -> UUID_PATTERN.matcher(directory.getFileName().toString()).matches())
-          .filter(Files::isExecutable)
-          .filter(directory -> Files.exists(directory.resolve("Data/Library/Plates/index.xml")))
-          .findAny()
-          .orElseThrow(() -> new IllegalStateException("No candidate SkyDemon directory found in " + dataDir));
-
-      return candidateDirectory.resolve("Data/Library/Plates");
-    }
-  }
-
-  private static Path findSkyDemonDataForWindows() throws IOException {
-    Path indexLocation = Paths.get(System.getenv("APPDATA"), "Divelements Limited", "SkyDemon Plan", "Plates", "index.xml");
-
-    if (!Files.exists(indexLocation)) {
-      return indexLocation.getParent();
-    }
-
-    throw new IllegalStateException("No SkyDemon data found in " + indexLocation.getParent());
-  }
-
-  private static Properties readAirfieldNames() {
-    Properties properties = new Properties();
-    try {
-      properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("airfields.properties"));
-    } catch (IOException e) {
-      throw new UncheckedIOException(e);
-    }
-    return properties;
-  }
-
-  private static String expandUserHome(String path) {
-    return path.replaceFirst("^~", System.getProperty("user.home"));
-  }
 }
